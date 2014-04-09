143054ce1917d178158865c41d0c5c8db320de0e
joel <joel@Elizabeth.(none)>
2014-04-09 09:58:58 -0500

    changing location of files 	deleted:    Screen/admin.php 	deleted:    Screen/css/admin.css 	deleted:    Screen/css/index.css 	deleted:    Screen/css/login.css 	deleted:    Screen/images/Weather/00.png 	deleted:    Screen/images/Weather/01.png 	deleted:    Screen/images/Weather/02.png 	deleted:    Screen/images/Weather/03.png 	deleted:    Screen/images/Weather/04.png 	deleted:    Screen/images/Weather/05.png 	deleted:    Screen/images/Weather/06.png 	deleted:    Screen/images/Weather/07.png 	deleted:    Screen/images/Weather/08.png 	deleted:    Screen/images/Weather/09.png 	deleted:    Screen/images/Weather/10.png 	deleted:    Screen/images/Weather/11.png 	deleted:    Screen/images/Weather/12.png 	deleted:    Screen/images/Weather/13.png 	deleted:    Screen/images/Weather/14.png 	deleted:    Screen/images/Weather/15.png 	deleted:    Screen/images/Weather/16.png 	deleted:    Screen/images/Weather/17.png 	deleted:    Screen/images/Weather/18.png 	deleted:    Screen/images/Weather/19.png 	deleted:    Screen/images/Weather/20.png 	deleted:    Screen/images/Weather/21.png 	deleted:    Screen/images/Weather/22.png 	deleted:    Screen/images/Weather/23.png 	deleted:    Screen/images/Weather/24.png 	deleted:    Screen/images/Weather/25.png 	deleted:    Screen/images/Weather/26.png 	deleted:    Screen/images/Weather/27.png 	deleted:    Screen/images/Weather/28.png 	deleted:    Screen/images/Weather/29.png 	deleted:    Screen/images/Weather/30.png 	deleted:    Screen/images/Weather/31.png 	deleted:    Screen/images/Weather/32.png 	deleted:    Screen/images/Weather/33.png 	deleted:    Screen/images/Weather/34.png 	deleted:    Screen/images/Weather/35.png 	deleted:    Screen/images/Weather/36.png 	deleted:    Screen/images/Weather/37.png 	deleted:    Screen/images/Weather/38.png 	deleted:    Screen/images/Weather/39.png 	deleted:    Screen/images/Weather/40.png 	deleted:    Screen/images/Weather/41.png 	deleted:    Screen/images/Weather/42.png 	deleted:    Screen/images/Weather/43.png 	deleted:    Screen/images/Weather/44.png 	deleted:    Screen/images/Weather/45.png 	deleted:    Screen/images/Weather/46.png 	deleted:    Screen/images/Weather/47.png 	deleted:    Screen/images/Weather/background.jpg 	deleted:    Screen/images/down.png 	deleted:    Screen/images/loader.gif 	deleted:    Screen/images/logo.jpg 	deleted:    Screen/images/logo.png 	deleted:    Screen/images/up.png 	deleted:    Screen/images/uploads/PalavraAmiga.jpg 	deleted:    Screen/images/uploads/example.jpg 	deleted:    Screen/images/x.png 	deleted:    Screen/index.html 	deleted:    Screen/js/index.js 	deleted:    Screen/js/jquery-1.10.2.min.js 	deleted:    Screen/js/jquery-1.10.2.min.map 	deleted:    Screen/js/jquery.form.min.js 	deleted:    Screen/login.php 	deleted:    Screen/php/SimplePie.Weather.php 	deleted:    Screen/php/SimplePie.compiled.php 	deleted:    Screen/php/admin.php 	deleted:    Screen/php/conn.php 	deleted:    Screen/php/file_upload.php 	deleted:    Screen/php/refresh.php 	admin.php 	css/ 	images/ 	index.html 	js/ 	login.php 	php/ 	uploads/

--- Screen/php/conn.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$link = mysql_connect("localhost", "mysql");
-if (!$link) { die('Could not connect: ' . mysql_error()); }
-
-$db_selected = mysql_select_db('screen', $link);
-if (!$db_selected) { die ('Can\'t use screen database : ' . mysql_error()); }
-
-?>
